96ba9b09ded615cf5d128c79c9b920e4f0f91677
arturo32 <arturofonsecas32@gmail.com>
2020-03-29 19:32:39 -0300

    Change getMethod function
    I changed the format that the response data of the GET method was
    being received. It is now json format instead of text. This makes
    searching for parameters much more easier, with the reponse now
    being a object with paramers instead of a long string that requires
    RegEx to be used!

--- ponteiros.js
+++ ponteiros.js
@@ -319,76 +319,64 @@ $(document).ready( function(){
 	    	},
 
 	   		function(data) {
-				getMethod(data);	
-			}, "text"); 
+				getMethod(data.id);	
+			}, "json"); 
 	});
 });
 
 /*This functions sends a GET request to the Paiza server to get the output of the user code. 
 It carries the ID information, passed by the response of the POST request.*/
-function getMethod(d){
+function getMethod(api_id){
 
-	/*From the data that was passed by the POST request it's selected only the ID 
-	information. This is made by a regular expression that matches whatever is inside
-	quotation marks after the "id:" word. Then the quotation marks are "replaced" by
-	empty strings.*/
 	var getData = new Object();
-	var idRegex = /"[^(id):].*?"/i;
-	getData.id = d.match(idRegex).toString().replace(/"/g, "");
+	getData.id = api_id;
 	getData.api_key = "guest";
     $.get("https://api.paiza.io/runners/get_details", getData, function(data){
 
-    	/*To check if the code has finished compiling, it's searched in the response data,
-    	using regular expressions, if the status parameter is still marked as "running".
-    	If so, then the getMethod function is called again after 100 milliseconds.*/
-		var status = /"status": ".*?"/i;
-		if(data.match(status).toString() == "\"status\": \"running\""){
+    	/*To check if the code has finished compiling, it's checked if the status parameter
+    	is still marked as "running". If so, then the getMethod function is called again
+    	after 100 milliseconds.*/
+		console.log(data.status);
+		if(data.status == "running"){
 			console.log("again");
-			setTimeout(function(){getMethod(d)}, 100);
+			setTimeout(function(){getMethod(api_id)}, 100);
 			return;
 		}
 
-		/*If the status is marked as completed, then it's searched in the response data the
-		stdout parameter, that contains the output of the user code. Since everything is
-		converted in a string, the \n commands will not be read as line breaks in the outputC
-		div element. So they are replaced by \n commands. It may be the case that there is no
-		stdout data due to errors in compilation or execution. So, before anything, errors 
-		messages are searched and, if found, sent to the user*/
+		/*If the status is marked as completed, then is used the stdout parameter, that contains
+		the output of the user code. It may be the case that there is no stdout data due to errors
+		in compilation or execution. So, before anything, errors messages are searched and, if
+		found, sent to the user*/
 
 		//"build_stderr" is the parameter of errors in compilation
-		let build_stderr = /"build_stderr": ".*?"/;
-		let compilationError = data.match(build_stderr).toString().replace(/"build_stderr": /, "").replace(/"/g, "").replace(/\\n/g, "\n");
+		let compilationError = data.build_stderr;
 		
 		/*The "result" parameter tells if something went wrong on execution. It can have the 
 		value "success" when everything went fine, "failure" when something went wrong on
 		execution or null when something went wrong before execution, like a compilation
 		error*/
-		let result =  /"result": (?:"\w+"|null)/;
-		let executionError = data.match(result).toString().replace(/"result": /, "").replace(/"/g, "").replace(/\\n/g, "\n");
+		let executionError = data.result;
 
 		//If there is a message of compilation error then it is sent to the outputC element
 		if(compilationError != ""){
 			document.getElementById("outputC").innerHTML = "ERRO DE COMPILAÇÃO:\n" + compilationError;
 		}
 		
-		/*If an execution error occurred, then it is searched, in the response, the "exit_code"
-		parameter. Its value represents the kind of error that happened and it is sent to
-		the outputC element*/
+		/*If an execution error occurred, then the "exit_code" parameter will be used. Its value
+		represents the kind of error that happened and it is sent to the outputC element*/
 		else if(executionError == "failure"){
-			let exit_code = /"exit_code": [0-9]*/;
-			executionError = data.match(exit_code).toString().replace(/"/g, "").replace(/\\n/g, "\n");
+			executionError = data.exit_code;
 			document.getElementById("outputC").innerHTML = "ERRO DE EXECUÇÃO:\n" + executionError;
 		}
 
 		/*If none of the errors occurred, it is sent the value of the "stdout" parameter that
 		contains the output of the code*/
 		else{
-			let stdout = /"stdout": ".*?"/;
-			let outputC = data.match(stdout).toString().replace(/"stdout":/, "").replace(/"/g, "").replace(/\\n/g, "\n");
+			let outputC = data.stdout;
 			document.getElementById("outputC").innerHTML = outputC;
 		}
 
-    }, "text");  
+    }, "json");  
 }
 
 
