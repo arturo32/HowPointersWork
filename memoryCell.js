eff670f64e3fbe9de46b76a8fdc5bbf5153c1714
Arturo Fonseca <arturofonsecas32@gmail.com>
2024-11-22 11:45:02 -0300

    add: improve design

--- memoryCell.js
+++ memoryCell.js
@@ -1,7 +1,8 @@
 export default {
-	template: `<div :class="'memory-cell ' + 'memory-cell-' + variable[1][1]">
+	template: `<div :class="'memory-cell ' + 'memory-cell-' + variable[1][1] + ' ' + (variable[1][2][0] === '*' ? 'pointer' : variable[1][2])">
 					<div class="address">{{ variable[1][1] }}</div>	
 					<div class="content">{{ variable[1][3] }}</div>	
+					<div class="type">{{ variable[1][2] }}</div>
 					<div class="name">{{ variable[0] }}</div>	
 				</div>`,
 	data() {
@@ -16,24 +17,31 @@ export default {
 		}
 	},
 	mounted() {
-		if(this.variable[1][2] === 'pointer') {
+		if(this.variable[1][2][0] === '*' || this.variable[1][2] === 'pointer') {
 			const pointerCell = document.querySelector('.memory-cell-' + this.variable[1][1]);
 			const pointedCell =  document.querySelector('.memory-cell-' + this.variable[1][3]);
-			globalArrows.push(new LeaderLine(
-				pointerCell,
-				pointedCell,
-				{
-					path: 'fluid',
-					startSocket: 'right',
-					endSocket: 'right',
-					startSocketGravity: [20, 0],
-					endSocketGravity: [20, 0],
-					endPlug: 'arrow3',
-					size: 2,
-					endPlugSize: 2,
-					color: getComputedStyle(document.body).getPropertyValue('--cell-border')
-				}
-			));
+			if(pointedCell !== null) {
+				globalArrows.push(new LeaderLine(
+					pointerCell,
+					pointedCell,
+					{
+						path: 'fluid',
+						startSocket: 'right',
+						endSocket: 'right',
+						startSocketGravity: [20, 0],
+						endSocketGravity: [20, 0],
+						endPlug: 'arrow3',
+						size: 2,
+						endPlugSize: 2,
+						color: getComputedStyle(document.body).getPropertyValue('--cell-border')
+					}
+				));
+			}
+		}
+	},
+	methods: {
+		createPointerArrow() {
+
 		}
 	}
 }
--- memoryContainer.js
+++ memoryContainer.js
@@ -1,11 +1,12 @@
 import Memory from "./memory.js";
+
 const { createApp } = Vue;
 
 const vm = createApp({
 	data() {
 		return {
 			message: 'Hello Vues!',
-			codeTrace: []
+			stack: []
 		}
 	},
 	components: {
@@ -30,8 +31,11 @@ const vm = createApp({
 				}
 
 				const json = await response.json();
-				globalArrows = []
-				this.codeTrace = this.extractLocals(json);
+				for(let line of globalArrows) {
+					line.remove();
+				}
+				globalArrows = [];
+				this.stack = this.extractLocals(json);
 			} catch (error) {
 				console.error(error.message);
 			}
@@ -41,12 +45,45 @@ const vm = createApp({
 			const locals = lastLineState.stack_to_render[0].encoded_locals;
 
 			// Transforms [{varName: properties}] into [["varname", properties]]
-			const localsArray = Object.entries(locals);
+			const localsArray = new Array(...Object.entries(locals));
 
 			// Ordering by address
 			localsArray.sort((a, b) => a[1][1].localeCompare(b[1][1]));
 
+			// Changing type of pointers from "pointer" to "*<type>"
+			for(const cell of localsArray) {
+				if(cell[1][2] === 'pointer') {
+					const address = cell[1][3];
+					const pointedVariable = localsArray.find(pointedCell => pointedCell[1][1] === address);
+					if(pointedVariable !== undefined) {
+						cell[1][2] = '*' + pointedVariable[1][2];
+					} else {
+						cell[1][2] = 'pointer';
+					}
+				}
+			}
+
+			if(localsArray.length < 20) {
+				return this.addAdjacentCells(localsArray);
+			}
+
 			return localsArray;
+		},
+		addAdjacentCells(cells) {
+			//const firstAddress = parseInt(cells[0][1][1], 16);
+			const lastAddress = parseInt(cells[cells.length - 1][1][1], 16);
+
+			const previousCells = [];
+			// for(let i = 5; i > 0; --i) {
+			// 	const newAddress = firstAddress - i;
+			// 	previousCells.push(['', ['C_DATA', '0x' + newAddress.toString(16).toUpperCase(), 'lixo', Math.floor(Math.random()*254).toString(2)]]);
+			// }
+			const posteriorCells = [];
+			for(let i = 1; i <= 10; ++i) {
+				const newAddress = lastAddress + i;
+				posteriorCells.push(['', ['C_DATA', '0x' + newAddress.toString(16).toUpperCase(), 'lixo',  Math.floor(Math.random()*254).toString(2)]]);
+			}
+			return previousCells.concat(cells).concat(posteriorCells);
 		}
 	},
 
