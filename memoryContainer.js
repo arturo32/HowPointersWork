c45cb85070f852a4f739c85273f3d9d1368abae0
Arturo Fonseca <arturofonsecas32@gmail.com>
2024-11-26 18:46:07 -0300

    add: arrays representation

--- memoryContainer.js
+++ memoryContainer.js
@@ -50,10 +50,8 @@ const vm = createApp({
 			// Transforms [{varName: properties}] into [["varname", properties]]
 			const localsArray = new Array(...Object.entries(locals));
 
-			// Ordering by address
-			localsArray.sort((a, b) => a[1][1].localeCompare(b[1][1]));
 
-
+			let arrayCells = [];
 			for(const cell of localsArray) {
 				// Changing type of pointers from "pointer" to "*<type>"
 				if(cell[1][2] === 'pointer') {
@@ -70,29 +68,47 @@ const vm = createApp({
 				if(cell[1][3] === '<UNINITIALIZED>') {
 					cell[1][3] = '?';
 				}
+				if(cell[1][2].constructor === Array && cell[1][2][3] === '<UNINITIALIZED>') {
+					cell[1][2][3] = '?';
+				}
+
+				if(cell[1][0] === 'C_ARRAY') {
+					this.flatArray(cell, arrayCells)
+				}
 			}
+			localsArray.push(...arrayCells);
+
+			// Ordering by address
+			localsArray.sort((a, b) => a[1][1].localeCompare(b[1][1]));
 
 			if(localsArray.length < 20) {
-				return this.addAdjacentCells(localsArray);
+				return this.addUnitilizedCells(localsArray);
 			}
 
 			return localsArray;
 		},
-		addAdjacentCells(cells) {
-			//const firstAddress = parseInt(cells[0][1][1], 16);
+		flatArray(cell, arrayCells) {
+			let i = 3;
+			while(cell[1][i] !== undefined) {
+				if(cell[1][i][3] === '<UNINITIALIZED>') {
+					cell[1][i][3] = '?';
+				}
+				arrayCells.push(['', cell[1][i]]);
+				if(cell[1][i][0] === 'C_ARRAY') {
+					this.flatArray(cell, arrayCells)
+				}
+				++i;
+			}
+		},
+		addUnitilizedCells(cells) {
 			const lastAddress = parseInt(cells[cells.length - 1][1][1], 16);
 
-			const previousCells = [];
-			// for(let i = 5; i > 0; --i) {
-			// 	const newAddress = firstAddress - i;
-			// 	previousCells.push(['', ['C_DATA', '0x' + newAddress.toString(16).toUpperCase(), 'lixo', Math.floor(Math.random()*254).toString(2)]]);
-			// }
 			const posteriorCells = [];
 			for(let i = 1; i <= 10; ++i) {
 				const newAddress = lastAddress + i;
-				posteriorCells.push(['', ['C_DATA', '0x' + newAddress.toString(16).toUpperCase(), 'lixo',  Math.floor(Math.random()*254).toString(2)]]);
+				posteriorCells.push(['', ['C_DATA', '0x' + newAddress.toString(16).toUpperCase(), 'lixo',  '?']]);
 			}
-			return previousCells.concat(cells).concat(posteriorCells);
+			return cells.concat(posteriorCells);
 		},
 		extractHeap() {
 			const lastLineState = this.json.trace[this.currentLine];
@@ -101,16 +117,19 @@ const vm = createApp({
 			// Transforms [{varName: properties}] into [["varname", properties]]
 			const heapArray = new Array(...Object.entries(heap));
 
-			// Ordering by address
-			heapArray.sort((a, b) => a[1][1].localeCompare(b[1][1]));
 
+			let arrayCells = [];
 			for(let cell of heapArray) {
 				cell[0] = '';
 				if(cell[1][2][3] === '<UNINITIALIZED>') {
 					cell[1][2][3] = '?';
 				}
+				this.flatArray(cell, arrayCells);
 			}
+			heapArray.push(...arrayCells);
 
+			// Ordering by address
+			heapArray.sort((a, b) => a[1][1].localeCompare(b[1][1]));
 
 			return heapArray;
 		},
@@ -120,6 +139,7 @@ const vm = createApp({
 				this.heap = this.extractHeap();
 				this.stack = this.extractLocals();
 				editor.gotoLine(this.json.trace[this.currentLine].line);
+				document.querySelector('#outputC').textContent = this.json.trace[this.currentLine].stdout;
 			}
 		},
 		previousLine() {
@@ -128,6 +148,7 @@ const vm = createApp({
 				this.heap = this.extractHeap();
 				this.stack = this.extractLocals();
 				editor.gotoLine(this.json.trace[this.currentLine].line);
+				document.querySelector('#outputC').textContent = this.json.trace[this.currentLine].stdout;
 			}
 		}
 	},
